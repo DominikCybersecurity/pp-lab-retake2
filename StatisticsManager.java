7a56ec52f05ac3dc35bd1d9d347814be748b0685
Dominik Sulima <166212630+DominikCybersecurity@users.noreply.github.com>
2024-06-12 21:35:27 +0200

    StatisticsManager.java

---
+++ StatisticsManager.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class StatisticsManager implements StatisticsOperations {
+    private List<DataSet> dataSets = new ArrayList<>();
+
+    public void addDataSet(DataSet dataSet) { dataSets.add(dataSet); }
+    public void removeDataSet(String name) { dataSets.removeIf(dataSet -> dataSet.getName().equals(name)); }
+    public DataSet findDataSetByName(String name) { return dataSets.stream().filter(dataSet -> dataSet.getName().equals(name)).findFirst().orElse(null); }
+    public void addNumberToDataSet(String name, double number) { findDataSetByName(name).addNumber(number); }
+    public void removeNumberFromDataSet(String name, double number) { findDataSetByName(name).removeNumber(number); }
+    public void displayDataSet(String name) { System.out.println(findDataSetByName(name)); }
+
+    @Override
+    public double calculateMean(DataSet dataSet) {
+        return dataSet.getData().stream().mapToDouble(Double::doubleValue).average().orElse(0);
+    }
+
+    @Override
+    public double calculateMedian(DataSet dataSet) {
+        List<Double> data = new ArrayList<>(dataSet.getData());
+        Collections.sort(data);
+        int middle = data.size() / 2;
+        return data.size() % 2 == 0 ? (data.get(middle - 1) + data.get(middle)) / 2.0 : data.get(middle);
+    }
+
+    @Override
+    public double calculateStandardDeviation(DataSet dataSet) {
+        double mean = calculateMean(dataSet);
+        return Math.sqrt(dataSet.getData().stream().mapToDouble(num -> Math.pow(num - mean, 2)).sum() / dataSet.getData().size());
+    }
+}
